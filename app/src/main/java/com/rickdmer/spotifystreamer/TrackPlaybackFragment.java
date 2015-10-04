fde830d5b1c879217a32b0efd4b74ce67a7f17a0
rickdmer <rickdmer@gmail.com>
2015-10-04 18:59:33 -0400

    Rig up pause/play, duration, and scrub bar

--- app/src/main/java/com/rickdmer/spotifystreamer/TrackPlaybackFragment.java
+++ app/src/main/java/com/rickdmer/spotifystreamer/TrackPlaybackFragment.java
@@ -1,80 +1,122 @@
 package com.rickdmer.spotifystreamer;
 
 import android.app.DialogFragment;
-import android.app.Fragment;
 import android.content.Intent;
 import android.media.AudioManager;
-import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Richard on 9/30/2015.
  */
 public class TrackPlaybackFragment extends DialogFragment {
 
+    MediaPlayer mediaPlayer = new MediaPlayer();
+    ImageButton imageButtonPlayPause;
+    TextView textViewArtistName;
+    TextView textViewAlbumName;
+    TextView textViewTrackName;
+    ImageView imageViewAlbumArt;
+    TextView textViewSongLength;
+    TextView textViewCurrentSongProgress;
+    SeekBar seekBar;
+    View rootView;
+    Handler seekbarHandler;
+
+    private void loadTrack(String trackUrl) throws IOException {
+        mediaPlayer.reset();
+        imageButtonPlayPause.setImageResource(android.R.drawable.ic_media_play);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDataSource(trackUrl);
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer _mediaPlayer) {
+                final long trackLength = mediaPlayer.getDuration();
+
+                // method from http://stackoverflow.com/a/10874133
+                int seconds = (int) (trackLength / 1000) % 60;
+                int minutes = (int) ((trackLength / (1000 * 60)) % 60);
+
+                String duration = String.valueOf(minutes) + ":" + String.valueOf(seconds);
+
+                seekBar.setMax(seconds);
+                seekbarHandler = new Handler();
+                if (getActivity() != null) {
+                    getActivity().runOnUiThread(seekBarRunnable);
+                }
+                textViewSongLength.setText(duration);
+            }
+        });
+        mediaPlayer.prepareAsync();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_player, container, false);
-        TextView textViewArtistName;
-        TextView textViewAlbumName;
-        TextView textViewTrackName;
-        TextView textViewSongLength;
-        ImageView imageViewAlbumArt;
-        ImageButton imageButtonPausePlay;
+        rootView = inflater.inflate(R.layout.fragment_player, container, false);
+        textViewArtistName = (TextView) rootView.findViewById(R.id.textViewArtistName);
+        textViewAlbumName = (TextView) rootView.findViewById(R.id.textViewAlbumName);
+        textViewTrackName = (TextView) rootView.findViewById(R.id.textViewSong);
+        imageViewAlbumArt = (ImageView) rootView.findViewById(R.id.imageViewAlbumArt);
+        imageButtonPlayPause = (ImageButton) rootView.findViewById(R.id.imageButtonPlayPause);
+        textViewCurrentSongProgress = (TextView) rootView.findViewById(R.id.textViewCurrentSongProgress);
+        textViewSongLength = (TextView) rootView.findViewById((R.id.textViewSongLength));
+        seekBar = (SeekBar) rootView.findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (mediaPlayer != null && fromUser) {
+                    mediaPlayer.seekTo(progress * 1000);
+                }
+            }
+        });
 
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra("artistName")) {
             String artistName = intent.getStringExtra("artistName");
-            textViewArtistName = (TextView) rootView.findViewById(R.id.textViewArtistName);
             textViewArtistName.setText(artistName);
         }
 
         if (intent != null && intent.hasExtra("albumName")) {
             String albumName = intent.getStringExtra("albumName");
-            textViewAlbumName = (TextView) rootView.findViewById(R.id.textViewAlbumName);
             textViewAlbumName.setText(albumName);
         }
 
         if (intent != null && intent.hasExtra("trackName")) {
             String trackName = intent.getStringExtra("trackName");
-            textViewTrackName = (TextView) rootView.findViewById(R.id.textViewSong);
             textViewTrackName.setText(trackName);
-        }
-
-        if (intent != null && intent.hasExtra("trackLength")) {
-            long trackLength = intent.getLongExtra("trackLength", 0);
-
-            // method from http://stackoverflow.com/a/10874133
-            int seconds = (int) (trackLength / 1000) % 60 ;
-            int minutes = (int) ((trackLength / (1000*60)) % 60);
-            int hours   = (int) ((trackLength / (1000*60*60)) % 24);
-
-            String duration = String.valueOf(minutes) + ":" + String.valueOf(seconds);
-            textViewSongLength = (TextView) rootView.findViewById((R.id.textViewSongLength));
-            textViewSongLength.setText(duration);
         }
 
         if (intent != null && intent.hasExtra("albumImageUrl")) {
             String albumImageUrl = intent.getStringExtra("albumImageUrl");
             if (Patterns.WEB_URL.matcher(albumImageUrl).matches()) {
-                imageViewAlbumArt = (ImageView) rootView.findViewById(R.id.imageViewAlbumArt);
                 Picasso.with(getActivity()).load(albumImageUrl).into(imageViewAlbumArt);
             } else {
                 Toast.makeText(getActivity(), R.string.invalid_image_url, Toast.LENGTH_SHORT).show();
@@ -83,34 +125,50 @@ public class TrackPlaybackFragment extends DialogFragment {
 
         if (intent != null && intent.hasExtra("trackPreviewUrl")) {
             final String trackPreviewUrl = intent.getStringExtra("trackPreviewUrl");
-            imageButtonPausePlay = (ImageButton) rootView.findViewById(R.id.imageButtonPlayPause);
-            imageButtonPausePlay.setOnClickListener(new View.OnClickListener() {
+
+            // load track
+            try {
+                loadTrack(trackPreviewUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            imageButtonPlayPause.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     try {
-                        playTrack(trackPreviewUrl);
+                        playPauseTrack(trackPreviewUrl);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
 
-                MediaPlayer mediaPlayer = new MediaPlayer();
+                private void playPauseTrack(String trackUrl) throws IOException {
 
-                private void playTrack(String trackUrl) throws IOException {
-                    mediaPlayer.reset();
-                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                    mediaPlayer.setDataSource(trackUrl);
-                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                        @Override
-                        public void onPrepared(MediaPlayer _mediaPlayer) {
-                            mediaPlayer.start();
-                        }
-                    });
-                    mediaPlayer.prepareAsync();
+                    if (mediaPlayer.isPlaying()) {
+                        imageButtonPlayPause.setImageResource(android.R.drawable.ic_media_play);
+                        mediaPlayer.pause();
+                    } else {
+                        imageButtonPlayPause.setImageResource(android.R.drawable.ic_media_pause);
+                        mediaPlayer.start();
+                    }
                 }
             });
         }
 
         return rootView;
     }
+
+    Runnable seekBarRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            if (mediaPlayer.isPlaying() && mediaPlayer.getCurrentPosition() <= mediaPlayer.getDuration()) {
+                int currentTime = (mediaPlayer.getCurrentPosition() / 1000) + 1;
+                seekBar.setProgress(currentTime);
+                textViewCurrentSongProgress.setText("0:" + String.format("%02d", currentTime));
+            }
+            seekbarHandler.postDelayed(this, 1000);
+        }
+    };
 }
